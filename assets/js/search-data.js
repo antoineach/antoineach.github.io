77697be8a2f3e3eb61b5f2bb4abf636ccf29e39b
antoineach <antoineach.stage1@gmail.com>
2025-10-28 10:36:23 +0000

    Deploying to gh-pages from @ antoineach/antoineach.github.io@33c901e95dafd93f71ea1e34b6d99b59f9d50aae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,17 +38,6 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2025/pointTransformerv2_en/";
-          
-        },
-      },{id: "post-point-transformer-v2-architecture-and-implementation-details-en",
-        
-          title: "Point Transformer v2: Architecture and Implementation Details en",
-        
-        description: "Detailed analysis of the Point Transformer v2 architecture for point-cloud segmentation and classification",
-        section: "Posts",
-        handler: () => {
-          
             window.location.href = "/blog/2025/pointTransformerv2/";
           
         },
